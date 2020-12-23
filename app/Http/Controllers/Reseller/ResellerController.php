55f8480dbfcbb5f468a9c171d357c8f0ee6ab3f4
deweks0 <lutfianjr1029f@gmail.com>
2020-12-23 10:08:53 +0700

    fix login as reseller

--- app/Http/Controllers/Reseller/ResellerController.php
+++ app/Http/Controllers/Reseller/ResellerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Reseller;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 
@@ -26,19 +27,19 @@ class ResellerController extends Controller
     }
     public function switchAccount(Request $request)
     {
-        $user = User::find($request->user_id);
-        Cookie::queue(Cookie::make('reseller',auth()->user()->id));
-
+        Cookie::queue('reseller', auth()->user()->id);
         if (!auth()->user()->hasRole('super-admin-company') || auth()->user()->hasRole('admin')) {
             Cookie::queue(Cookie::forget('reseller'));
-        }
-
-        // dd(auth()->user()->hasRole('super-admin-company')); 
-        Auth::logout($user);
-        Auth::login($user);
-        if (!auth()->user()->hasRole('super-admin-company') || auth()->user()->hasRole('admin')) {
+            $user = User::find(Cookie::get('reseller'));
+            Auth::logout($user);
+            Auth::login($user);
             return redirect()->route('admin');
+        }else{
+            $user = User::find($request->user_id);
+            Auth::logout($user);
+            Auth::login($user);
         }
+   
         return redirect()->route('reseller');
     }
 }
